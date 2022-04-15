50d420decdfbe54a46909effed9673d79d1e7544
Nick Laws <nick.laws@nrel.gov>
2022-04-15 15:16:36 -0600

    Update models.py

--- pyapi/job/models.py
+++ pyapi/job/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Job(models.Model):
     job_uuid = models.UUIDField(unique=True)
     status = models.TextField(blank=False)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
+    valid_input_int = models.IntegerField(default=1)
 
     @classmethod
     def create(cls, **kwargs):
