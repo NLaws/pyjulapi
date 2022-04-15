b7dc37690008fe06030a4370c372da9a69c743d4
Nick Laws <nick.laws@nrel.gov>
2022-04-15 16:32:48 -0600

    Update models.py

--- pyapi/job/models.py
+++ pyapi/job/models.py
@@ -7,6 +7,7 @@ class Job(models.Model):
     status = models.TextField(blank=False)
     created = models.DateTimeField(default=timezone.now)
     valid_input_int = models.IntegerField(default=1)
+    result = models.JSONField(default=dict)
 
     @classmethod
     def create(cls, **kwargs):
